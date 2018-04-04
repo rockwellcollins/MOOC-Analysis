1f15b7e84dd5154f7c749e5a86affc3dca037ced
Hongyan Gu <rockwellcollins@163.com>
2018-04-03 22:28:53 -0700

    initial commit

---
+++ grade_analysis.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 22 14:02:41 2018
+
+@author: kgicmd
+"""
+
+ROOT_PATH = "D:\\shx\\"
+FILE_PATH = "moc_term_score_summary\\"
+import os
+os.chdir(ROOT_PATH)
+
+import codecs
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+# statistics for users
+os.chdir(ROOT_PATH + FILE_PATH)
+
+f = open("000000_0","r")
+line = f.readline()
+print(line)
+f.close()
+
+
+def test_score_parse(file_path):
+    """
+    parse files of users testing information
+    """
+    data = np.zeros((1,20))
+    f = open(file_path, "r",encoding="utf8")
+    for line in f.readlines():
+        line_split = line.strip("\n").split("\x01")
+        try:
+            data = np.vstack((data, line_split))
+        except ValueError:
+            print("check this:")
+            print(line_split)
+    f.close()
+    data = data[1:,]
+    
+    return data
+
+for file_name in os.listdir(ROOT_PATH + FILE_PATH):
+    if file_name[0] == "0":
+        file_path = ROOT_PATH + FILE_PATH + file_name
+        data_tmp = test_score_parse(file_path)
+        
+data_tmp = pd.DataFrame(data_tmp)
+
+data_tmp.columns = ["id",
+                    "gmt_create",
+                    "gmt_modified",
+                    "term_id",
+                    "member_id",
+                    "test_score",
+                    "assignment_score",
+                    "exam_score",
+                    "discuss_score",
+                    "outside_score",
+                    "total_score",
+                    "tc_uptime",
+                    "bonus_score",
+                    "total_score_plus_bonus",
+                    "cert_send_status",
+                    "chargecert_mark",
+                    "charge_cert_send_status",
+                    "count_reply",
+                    "count_vote",
+                    "time" # I don't know what this is, check w/ admin
+                    ]
+
+data_tmp.to_csv("moc_term_grade_summary.csv")
---
+++ usr_analysis.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 22 12:48:56 2018
+
+@author: kgicmd
+"""
+
+ROOT_PATH = "D:\\shx\\"
+FILE_PATH = "user_tag_value\\"
+import os
+os.chdir(ROOT_PATH)
+
+import codecs
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+# statistics for users
+os.chdir(ROOT_PATH + FILE_PATH)
+
+#f = open("000021_0", "r")
+#line = f.readline()
+#print(line)
+#f.close()
+
+
+def user_file_parse(file_path):
+    """
+    parse files for users information
+    """
+    data = np.zeros((1,13))
+    f = open(file_path, "r",encoding="utf8")
+    for line in f.readlines():
+        line_split = line.replace('\x01',',').strip("\n").split(",")
+        try:
+            data = np.vstack((data, line_split))
+        except ValueError:
+            print("check this:")
+            print(line_split)
+    f.close()
+    data = data[1:,]
+    
+    return data
+
+tmp = np.zeros((1,13))
+for file_name in os.listdir(ROOT_PATH + FILE_PATH):
+    if file_name[0] == "0":
+        print(file_name)
+        file_path = ROOT_PATH + FILE_PATH + file_name
+        data_tmp = user_file_parse(file_path)
+        tmp = np.vstack((tmp, data_tmp))
+
+tmp = tmp[1:,]
+data_all = pd.DataFrame(tmp)
+data_all.columns = ["user_id",
+                    "user_nickname",
+                    "email",
+                    "birthday",
+                    "gender",
+                    "country",
+                    "province",
+                    "city",
+                    "sign_up_since", # I'm not quite sure what exactly this means
+                    "last_login_time",
+                    "term_id",
+                    "course_id",
+                    "enroll_time"]
+
+# split overall data according to course_id
+course_id_list = np.unique(data_all["course_id"])
+for course_id in course_id_list:
+    data_slice = data_all[data_all["course_id"] == course_id]
+    name = "users_of_" + str(course_id) + ".csv"
+    data_slice.to_csv(name)
+    
+data_all = data_all.drop_duplicates()
+data_all.to_csv("data_all.csv")
